a96adee658be0b1828f19e28f10f1bbf3eaf8486
Iolanda Rosa <2161477@my.ipleiria.pt>
2018-12-08 15:10:49 +0000

    Commit of the project to merge with master

---
+++ ProjectDAE-war/src/java/web/DownloadManager.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package web;
+
+import java.io.InputStream;
+import java.util.logging.Logger;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+
+/**
+ *
+ * @author Iolanda
+ */
+@ManagedBean
+public class DownloadManager {
+
+    
+
+    public DownloadManager() {
+        
+    }
+
+   
+
+}
